77fce1d9b9ea9629a39c2e779ded34a2c42d187b
Robert Zenz <robert.zenz@sibvisions.com>
2016-09-14 16:52:17 +0200

    Colors are now differing per row.

--- src/com/sibvisions/kitchensink/samples/layouts/GridLayoutSample.java
+++ src/com/sibvisions/kitchensink/samples/layouts/GridLayoutSample.java
@@ -73,24 +73,24 @@ public class GridLayoutSample extends AbstractSample implements ISample
 		main.add(createCenteredLabel("3, 0", Tango.PLUM_1), mainLayout.getConstraints(3, 0));
 		main.add(createCenteredLabel("4, 0", Tango.SCARLET_RED_1), mainLayout.getConstraints(4, 0));
 		
-		main.add(createCenteredLabel("0, 1", Tango.BUTTER_1), mainLayout.getConstraints(0, 1));
-		main.add(createCenteredLabel("1, 1, 2, 1", Tango.CHAMELEON_1), mainLayout.getConstraints(1, 1, 2, 1));
-		main.add(createCenteredLabel("3, 1, 1, 2", Tango.PLUM_1), mainLayout.getConstraints(3, 1, 1, 2));
-		main.add(createCenteredLabel("4, 1", Tango.SCARLET_RED_1), mainLayout.getConstraints(4, 1));
+		main.add(createCenteredLabel("0, 1", Tango.BUTTER_2), mainLayout.getConstraints(0, 1));
+		main.add(createCenteredLabel("1, 1, 2, 1", Tango.CHAMELEON_2), mainLayout.getConstraints(1, 1, 2, 1));
+		main.add(createCenteredLabel("3, 1, 1, 2", Tango.PLUM_2), mainLayout.getConstraints(3, 1, 1, 2));
+		main.add(createCenteredLabel("4, 1", Tango.SCARLET_RED_2), mainLayout.getConstraints(4, 1));
 		
-		main.add(createCenteredLabel("0, 2, 3, 1", Tango.BUTTER_1), mainLayout.getConstraints(0, 2, 3, 1));
-		main.add(createCenteredLabel("4, 2", Tango.SCARLET_RED_1), mainLayout.getConstraints(4, 2));
+		main.add(createCenteredLabel("0, 2, 3, 1", Tango.BUTTER_3), mainLayout.getConstraints(0, 2, 3, 1));
+		main.add(createCenteredLabel("4, 2", Tango.SCARLET_RED_3), mainLayout.getConstraints(4, 2));
 		
 		main.add(createCenteredLabel("0, 3", Tango.BUTTER_1), mainLayout.getConstraints(0, 3));
 		main.add(createCenteredLabel("1, 3", Tango.CHAMELEON_1), mainLayout.getConstraints(1, 3));
 		main.add(createCenteredLabel("2, 3, 2, 1", Tango.PLUM_1), mainLayout.getConstraints(2, 3, 2, 1));
 		main.add(createCenteredLabel("4, 3", Tango.SCARLET_RED_1), mainLayout.getConstraints(4, 3));
 		
-		main.add(createCenteredLabel("0, 4", Tango.BUTTER_1), mainLayout.getConstraints(0, 4));
-		main.add(createCenteredLabel("1, 4", Tango.CHAMELEON_1), mainLayout.getConstraints(1, 4));
-		main.add(createCenteredLabel("2, 4", Tango.SKY_BLUE_1), mainLayout.getConstraints(2, 4));
-		main.add(createCenteredLabel("3, 4", Tango.PLUM_1), mainLayout.getConstraints(3, 4));
-		main.add(createCenteredLabel("4, 4", Tango.SCARLET_RED_1), mainLayout.getConstraints(4, 4));
+		main.add(createCenteredLabel("0, 4", Tango.BUTTER_2), mainLayout.getConstraints(0, 4));
+		main.add(createCenteredLabel("1, 4", Tango.CHAMELEON_2), mainLayout.getConstraints(1, 4));
+		main.add(createCenteredLabel("2, 4", Tango.SKY_BLUE_2), mainLayout.getConstraints(2, 4));
+		main.add(createCenteredLabel("3, 4", Tango.PLUM_2), mainLayout.getConstraints(3, 4));
+		main.add(createCenteredLabel("4, 4", Tango.SCARLET_RED_2), mainLayout.getConstraints(4, 4));
 		
 		IDataBook controlsBook = new MemDataBook();
 		controlsBook.getRowDefinition().addColumnDefinition(new ColumnDefinition("HORIZONTAL_GAP", new BigDecimalDataType()));
